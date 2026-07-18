4a27c4890bb56945189588051bb600e9c1e667e3
Marco <marco.tranquilli@dos.design>
2026-07-18 12:45:28 +0200

    Disable Russo SW reload redirect on GitHub Pages

--- sw-killer.js
+++ sw-killer.js
@@ -5,17 +5,9 @@
       if (!regs || regs.length === 0) return false;
       return Promise.all(regs.map((r) => r.unregister())).then(() => true);
     })
-    .then((hadSW) => {
-      if (!hadSW) return;
+    .then(() => {
       if ('caches' in window) {
         return caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))));
-      }
-    })
-    .then(() => {
-      const url = new URL(window.location.href);
-      if (!url.searchParams.has('swreset')) {
-        url.searchParams.set('swreset', '1');
-        window.location.replace(url.toString());
       }
     })
     .catch(() => {});
